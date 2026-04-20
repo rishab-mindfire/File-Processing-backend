afa8adf0723d2d94dd296616b877216da677c460
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-04-20 11:26:49 +0530

    fix : file name extenstion by .

--- dist/utils/randomId.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateCustomId = generateCustomId;
-// Generate a custom id
-function generateCustomId() {
-    return Date.now().toString(36) + Math.random().toString(36).substr(2);
-}
-//# sourceMappingURL=randomId.js.map

551133b6901cffa418918b44d5bd24bcbc37b9d8
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-04-20 12:52:36 +0530

    feat : remove project routes and hard codded scret

--- src/services/authGeneral.service.ts
+++ src/services/authGeneral.service.ts
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 export function generateToken(user: any) {
-  const secret = 'DLCEOeL8Xf5TMDBaWnFeVAL86GoAEwdRjERMdO84Dg5';
+  const secret = process.env.JWT_SECRETE;
 
   if (secret) return jwt.sign(user, secret, { expiresIn: 60 * 60 * 12 });
 }
 
 export function verifyTokenAndGetUser(token: any) {
-  const secret = 'DLCEOeL8Xf5TMDBaWnFeVAL86GoAEwdRjERMdO84Dg5';
+  const secret = process.env.JWT_SECRETE;
   if (!token) return null;
   try {
     if (secret) return jwt.verify(token, secret);
